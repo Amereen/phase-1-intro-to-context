533a30b37bf55a622fb9faaf843b70882d59e079
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-19 12:40:52 -0400

    commit tree d24ee44ca260058b1b6c122d207eefa2457a18ea

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+function createEmployeeRecord (array) {
+const object1 = {
+  firstName: array[0]   ,
+  familyName:  array[1]   ,
+  title:   array[2]   ,
+  payPerHour:  array[3]   ,
+  timeInEvents:    [] ,
+  timeOutEvents:   []   ,
+}
+return object1
+}
+
+function createEmployeeRecords(employees){
+return employees.map(createEmployeeRecord)
+
+}
+
+function createTimeInEvent(record,date){
+const obj2 = {
+  type: "TimeIn",
+  hour: parseInt(date.split(" ")[1]),
+  date: date.split(" ")[0],
+}
+record.timeInEvents.push(obj2)
+return record
+}
+function createTimeOutEvent(record, date){
+const obj3 = {
+  type: "TimeOut",
+  hour: parseInt(date.split(" ")[1]),
+  date: date.split(" ")[0],
+}
+record.timeOutEvents.push(obj3)
+return record
+}
+ 
+function hoursWorkedOnDate (record, date){
+const timeIN = record.timeInEvents
+const timeOut  = record.timeOutEvents
+ let time1 = timeIN.find(obj => obj.date === date) 
+ let time2 =  timeOut.find(obj => obj.date === date) 
+return Math.abs(time1.hour - time2.hour) / 100
+
+
+ //  console.log(time1.hour,time2.hour)
+
+}
+
+function wagesEarnedOnDate( record , date){
+ console.log(record,date)
+}
